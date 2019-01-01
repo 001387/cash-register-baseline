fc5f7bb9e33e99b913541b778dba8aa6fc0eac3f
LittleBell123 <415725257@qq.com>
2019-01-02 00:41:17 +0800

    cash-register-baseline-20190101

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -8,8 +8,13 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase() {
         //initialize CashRegister and fake Printer
-
-        cashRegister.process(purchase);
+    	Item item1 = new Item("Element 1", 27);
+    	Item item2 = new Item("Element 2", 77);
+    	Item[] items = new Item[] {item1, item2};
+    	Purchase purchase = new Purchase(items);
+    	
+    	CashRegister cashRegister = new CashRegister(null);
+        cashRegister.process(purchase);
 
         //verify that printer was called
     }
