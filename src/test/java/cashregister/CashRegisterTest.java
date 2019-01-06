7629f71f05561025f12de358e490768b3a97ea07
LittleBell123 <415725257@qq.com>
2019-01-07 00:36:29 +0800

    Remove “With”

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -8,14 +8,15 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase() {
         //initialize CashRegister and fake Printer
-    	Item item1 = new Item("Element 1", 27);
-    	Item item2 = new Item("Element 2", 77);
-    	Item[] items = new Item[] {item1, item2};
+    	Item[] items = new Item[] {
+    			new Item("Noodles", 10), 
+    			new Item("Apples", 20)};
     	Purchase purchase = new Purchase(items);
-    	
+    	MockPrinter mockPrinter = new MockPrinter(); 
     	CashRegister cashRegister = new CashRegister(null);
         cashRegister.process(purchase);
 
         //verify that printer was called
+        mockPrinter.verifyThatPrintWasCalled();
     }
 }
