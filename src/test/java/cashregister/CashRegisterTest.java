2d71f137604801ed094ab6dc69abe149a937162b
LittleBell123 <415725257@qq.com>
2019-01-07 01:48:42 +0800

    Finish

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,27 +1,58 @@
 package cashregister;
 
-import org.junit.Test;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CashRegisterTest {
 
+    @Mock
+    Purchase stubPurchaseMockito;
+
+    @Mock
+    Printer mockPrinterMockito;
+
+    @InjectMocks
+    CashRegister cashRegisterMockito;
 
     @Test
     public void should_print_the_real_purchase() {
-        //initialize CashRegister and fake Printer
-    	Item[] items = new Item[] {
-    			new Item("Noodles", 10), 
-    			new Item("Apples", 20)};
-    	Purchase purchase = new Purchase(items);
-    	MockPrinter mockPrinter = new MockPrinter(); 
-    	CashRegister cashRegister = new CashRegister(null);
+        Item[] items = new Item[] {
+                new Item("Noodles", 10),
+                new Item("Apples", 20)
+        };
+        Purchase purchase = new Purchase(items);
+        MockPrinter mockPrinter = new MockPrinter();
+        CashRegister cashRegister = new CashRegister(mockPrinter);
+
         cashRegister.process(purchase);
 
-        //verify that printer was called
         mockPrinter.verifyThatPrintWasCalled();
     }
-    
-    @Test 
+
+    @Test
     public void should_print_the_stub_purchase() {
-    	
+        StubPurchase stubPurchase = new StubPurchase("purchase as string");
+        MockPrinter mockPrinter = new MockPrinter();
+        CashRegister cashRegister = new CashRegister(mockPrinter);
+
+        cashRegister.process(stubPurchase);
+
+        mockPrinter.verifyThatPrintWasCalledWith("purchase as string");
+    }
+
+    @Test
+    public void should_print_the_stub_purchase_using_mockito() {
+        when(stubPurchaseMockito.asString()).thenReturn("purchase as string");
+
+        cashRegisterMockito.process(stubPurchaseMockito);
+
+        verify(mockPrinterMockito, times(1)).print("purchase as string");
     }
 }
